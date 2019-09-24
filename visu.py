44c5e067313c6da08253d4f6075486fcc3f1490e
Yoann FOREAU <yforeau@e3r1p4.42.fr>
2019-09-24 18:13:16 +0200

    Scale elements in the canvas
    This allows for scaled resizing. It works pretty well, but there
    are some issues with the resize_canvas function (the handler for
    the tkinter resizing event). First it is called at the beginning
    of the program, and I don't understand why. Also, the exit()
    function does not appear to work.

--- visu.py
+++ visu.py
@@ -5,7 +5,7 @@ from utils import *
 from time import *
 
 class vdata:
-    def __init__(self):
+    def __init__(self, col):
         # grid data
         self.grid_w = 0
         self.grid_h = 0
@@ -30,24 +30,56 @@ class vdata:
         self.ant_color = "red"
         # number of frames by ant movement (speed)
         self.framec = 100
+        # current coordinates and x,y increments for each ant
+        self.steps = []
+        self.step = 0
         # print grid option
         self.print_grid = True
+        # copy of colony class for convenience
+        self.col = col
 
-    def init_screen(self, col):
-        self.grid_w = col.maxx + 1
-        self.grid_h = col.maxy + 1
-        self.screen_w = self.win.winfo_screenwidth() / 2
-        self.screen_h = self.win.winfo_screenheight() / 2
-        self.side = min(self.screen_w / (self.grid_w + 2),\
-        self.screen_h / (self.grid_h + 2))
-        if self.side < 10:
+    def scale_canvas(self, event):
+        self.can.pack(side=TOP, fill=BOTH, expand=1)
+        if self.build_canvas_grid(event.width, event.height):
             eprint("error: map too big for the screen")
             exit()
+        self.draw_map()
+        # this shit is called before the game is even launched, whyyyy ?
+        self.draw_ants(turn=self.col.turn - 1 if self.col.turn > 0 else 0)
+        self.get_steps()
+        self.draw_step()
+        self.can.update()
+
+    def build_canvas_grid(self, width, height):
+        self.screen_w = width
+        self.screen_h = height
+        self.side = min(self.screen_w / (self.grid_w + 2),\
+        self.screen_h / (self.grid_h + 2))
         self.orig_x = (self.screen_w - (self.grid_w * self.side)) / 2
         self.orig_y = (self.screen_h - (self.grid_h * self.side)) / 2
+        if self.side < 10:
+            return 1
+        else:
+            return 0
+
+    def init_screen(self):
+        self.grid_w = self.col.maxx + 1
+        self.grid_h = self.col.maxy + 1
+        self.screen_w = self.win.winfo_screenwidth() / 2
+        self.screen_h = self.win.winfo_screenheight() / 2
+        # try to resize the grid to a minimum if it does not fit the screen
+        if self.build_canvas_grid(self.screen_w, self.screen_h):
+            self.screen_w = (self.grid_w + 2) * 10
+            self.screen_h = (self.grid_h + 2) * 10
+            self.build_canvas_grid(self.screen_w, self.screen_h)
+            if self.screen_w > self.win.winfo_screenwidth()\
+            or self.screen_h > self.win.winfo_screenheight():
+                eprint("error: map too big for the screen")
+                exit()
         self.can = Canvas(self.win, width=self.screen_w, height=self.screen_h,\
         bg=self.background_color)
-        self.can.pack(side=TOP, padx=5, pady=5)
+        self.can.pack(side=TOP, fill=BOTH, expand=1)
+        self.can.bind("<Configure>", self.scale_canvas)
     
     # compute the coordinates to draw the game from grid coordinates
     def grid_to_graphical(self, g_x, g_y):
@@ -106,88 +138,89 @@ class vdata:
         self.ants.clear()
 
     # draw the shapes of the game
-    def draw_links(self, r, col):
-        for l in col.rooms[r].links:
+    def draw_links(self, r):
+        for l in self.col.rooms[r].links:
             if l not in self.rooms:
-                x1, y1, x2, y2 = self.link_coords(col.rooms[r].x,\
-                col.rooms[r].y, col.rooms[l].x, col.rooms[l].y)
+                x1, y1, x2, y2 = self.link_coords(self.col.rooms[r].x,\
+                self.col.rooms[r].y, self.col.rooms[l].x, self.col.rooms[l].y)
                 link = self.can.create_line(x1, y1, x2, y2,\
                 fill=self.link_color, width=self.side/15)
                 self.links.append(link)
 
-    def draw_room(self, r, col):
-            x1, y1, x2, y2 = self.room_coords(col.rooms[r].x, col.rooms[r].y)
+    def draw_room(self, r):
+            x1, y1, x2, y2 = self.room_coords(self.col.rooms[r].x, self.col.rooms[r].y)
             room = self.can.create_oval(x1, y1, x2, y2, fill=self.room_color)
             self.rooms[r] = room
 
-    def draw_map(self, col):
+    def draw_map(self):
         self.delete_map()
         if self.print_grid:
             self.draw_grid()
-        for r in col.rooms:
-            self.draw_links(r, col)
-            self.draw_room(r, col)
+        for r in self.col.rooms:
+            self.draw_links(r)
+            self.draw_room(r)
         self.can.pack()
 
-    # draw ants at current postition (according to col.turn)
-    def draw_ants(self, col):
+    # draw ants at current postition (according to self.col.turn)
+    def draw_ants(self, turn=0):
         self.delete_ants()
-        for i in range(col.antn):
-            r = col.game[col.turn][i]
-            x1, y1, x2, y2 = self.ant_coords(col.rooms[r].x, col.rooms[r].y)
+        for i in range(self.col.antn):
+            r = self.col.game[turn][i]
+            x1, y1, x2, y2 = self.ant_coords(self.col.rooms[r].x, self.col.rooms[r].y)
             ant = self.can.create_oval(x1, y1, x2, y2, fill=self.ant_color)
             self.ants.append(ant)
         self.can.pack()
 
     # animate the ants
-    def get_steps(self, col):
-        steps = []
-        for i in range(col.antn):
-            xy = self.ant_coords(col.rooms[col.game[col.turn][i]].x,\
-            col.rooms[col.game[col.turn][i]].y)
+    def get_steps(self):
+        self.steps.clear()
+        for i in range(self.col.antn):
+            xy = self.ant_coords(self.col.rooms[self.col.game[self.col.turn][i]].x,\
+            self.col.rooms[self.col.game[self.col.turn][i]].y)
             cur = self.can.coords(self.ants[i])
             ix = (xy[0] - cur[0]) / self.framec
             iy = (xy[1] - cur[1]) / self.framec
-            steps.append([cur[0], cur[1], cur[2], cur[3], ix, iy])
-        return steps
+            self.steps.append([cur[0], cur[1], cur[2], cur[3], ix, iy])
 
-    def move_ants(self, col):
+    def draw_step(self):
+        for j in range(self.col.antn):
+            # move ant one frame toward the next node
+            self.can.coords(self.ants[j],\
+            self.steps[j][0] + (self.steps[j][4] * self.step),\
+            self.steps[j][1] + (self.steps[j][5] * self.step),\
+            self.steps[j][2] + (self.steps[j][4] * self.step),\
+            self.steps[j][3] + (self.steps[j][5] * self.step))
+
+    def move_ants(self):
         # array of coordinates plus increment values
-        steps = self.get_steps(col)
-        for i in range(self.framec):
-            for j in range(col.antn):
-                if i < (self.framec - 1):
-                    # move ant toward the next node
-                    self.can.coords(self.ants[j],\
-                    steps[j][0] + (steps[j][4] * i),\
-                    steps[j][1] + (steps[j][5] * i),\
-                    steps[j][2] + (steps[j][4] * i),\
-                    steps[j][3] + (steps[j][5] * i))
-                else:
-                    # fix ant precisely on the node
-                    xy = self.ant_coords(col.rooms[col.game[col.turn][j]].x,\
-                    col.rooms[col.game[col.turn][j]].y)
-                    self.can.coords(self.ants[j], xy[0], xy[1], xy[2], xy[3])
+        self.get_steps()
+        self.step = 0
+        while self.step <= self.framec:
+            self.draw_step()
             self.can.pack()
             self.can.update()
-        steps.clear()
-
+            self.step += 1
+        for i in range(self.col.antn):
+            # fix ant precisely on the node
+            xy = self.ant_coords(self.col.rooms[self.col.game[self.col.turn][i]].x,\
+            self.col.rooms[self.col.game[self.col.turn][i]].y)
+            self.can.coords(self.ants[i], xy[0], xy[1], xy[2], xy[3])
 
 def play_game(col, vda):
     col.turn = 1
     while col.turn < len(col.game):
-        vda.move_ants(col)
+        vda.move_ants()
         sleep(0.3)
         col.turn += 1
 
 def run_visu(col):
     # init visual data
-    vda = vdata()
+    vda = vdata(col)
     col.normalize_coords()
-    vda.init_screen(col)
+    vda.init_screen()
 
     # draw game 
-    vda.draw_map(col)
-    vda.draw_ants(col)
+    vda.draw_map()
+    vda.draw_ants()
     vda.can.update()
     play_game(col, vda)
